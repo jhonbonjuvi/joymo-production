a6f861b0a8e2cf51aefb44c3e66aeec9028b36e4
jhonbonjuvi <johnbonjuvijumarito@gmail.com>
2023-01-27 10:27:48 +0800

    adding header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,28 +7,30 @@ export default component$(() => {
 
   return (
     <header>
-      <div class="logo">
-        <a href="https://qwik.builder.io/" target="_blank" title="qwik">
-          <QwikLogo />
-        </a>
-      </div>
-      <ul>
-        <li>
-          <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
-            Docs
+      <div class="bg-green-500 border-b border-black w-full">
+        <div class="logo bg-blue-500">
+          <a href="https://qwik.builder.io/" target="_blank" title="qwik">
+            <QwikLogo />
           </a>
-        </li>
-        <li>
-          <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
-            Examples
-          </a>
-        </li>
-        <li>
-          <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
-            Tutorials
-          </a>
-        </li>
-      </ul>
+        </div>
+        <ul>
+          <li>
+            <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
+              Docs
+            </a>
+          </li>
+          <li>
+            <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
+              Examples
+            </a>
+          </li>
+          <li>
+            <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
+              Tutorials
+            </a>
+          </li>
+        </ul>
+        </div>
     </header>
   );
 });
