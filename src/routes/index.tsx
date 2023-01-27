d1427e3f8efefd0d95443d92f6a4a667c5dfe355
jhonbonjuvi <johnbonjuvijumarito@gmail.com>
2023-01-27 10:49:31 +0800

    fixed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
+// import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
