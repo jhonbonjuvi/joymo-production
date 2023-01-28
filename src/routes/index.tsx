56515726ba81d1b61ba6f5122b256cb055636da3
John Bon Juvi Jumarito <123356180+jhonbonjuvi@users.noreply.github.com>
2023-01-28 22:57:01 +0800

    Update index.tsx
    updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import Upperbanner from '../../components/cover_banner';
 // import { Link } from '@builder.io/qwik-city';
+
 
 export default component$(() => {
   return (
     <div>
+        <Upperbanner/>
         <h1>
             Under Development
         </h1>
