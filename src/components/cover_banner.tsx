51e44d0b7587dc55d598d7e7a36d3a75270c9474
John Bon Juvi Jumarito <123356180+jhonbonjuvi@users.noreply.github.com>
2023-01-28 22:47:30 +0800

    Create cover_banner.tsx
    upper part

---
+++ src/components/cover_banner.tsx
@@ -0,0 +1,7 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return <>
+          <h1>We uncover the Joy and Happiness in every Experience</h1>;
+         </>
+});
